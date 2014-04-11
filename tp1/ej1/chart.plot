a5ce5e8997aef5edc24916034da40615eaac28a9
twisthl <twizt.hl@gmail.com>
2014-04-11 01:06:39 -0300

    test y chart ej1

---
+++ tp1/ej1/chart.plot
@@ -0,0 +1,9 @@
+set out "../informe/img/ej1_log(n)1.png"
+set xlabel "Cantidad de Camiones (Total)"
+set ylabel "Tiempo (clocks)"
+set key left top
+set terminal png size 1300,800 enhanced font "Helvetica,20"
+#set xrange [0:1e+06]
+#set yrange [0:1e+06]
+plot "datachart1.out" title "Datos aleatorios (seed 1)", "datachart1bis.out" title "Datos aleatorios (seed 2)", x*log(x)/log(2) title "O(n*log(n))"
+
---
+++ tp1/ej1/test.cpp
@@ -0,0 +1,47 @@
+#include "ej1.h"
+#include <time.h>
+#include <cmath>
+
+using namespace std;
+
+/**
+ * La función main toma el listado de trabajos por std input:
+ */
+int main(int argc, char *argv[])
+{
+	int seed = (int) *argv[1]; // Semilla para los rand()
+	int periodo;
+	LCamiones* camionesLista;
+	int nroCamiones;
+	int camionDia;
+	
+	if(seed>0){
+				
+		srand(seed);
+
+		periodo = rand() % 100000;
+		
+		// Tests aleatorios	
+		for(int i=0;i<100;i++){
+			camionesLista = new LCamiones();
+			nroCamiones = rand() % 10000000 + 1;
+			//periodo = nroCamiones;
+
+			for(int k=0;k<nroCamiones;k++){
+				camionDia = rand() % 10000000;
+				camionesLista->push_back(camionDia);
+			}
+			clock_t t = clock();
+			resolver(*camionesLista, periodo);
+			t = clock() - t;
+			float t_float = (float)t/log2f((float)nroCamiones);
+		
+			
+			cout << nroCamiones << "\t" << t_float << endl;
+			delete camionesLista;	
+		}
+	}
+	
+	return 0;
+	
+}
