8ba978e78b57264c22b4055eb53d564608820973
matguer <matias.guerson@gmail.com>
2014-05-06 10:01:52 -0300

    Se adapta el BFS para que reciba un rango de target

--- tp2/ej3/directed_graph.cpp
+++ tp2/ej3/directed_graph.cpp
@@ -56,53 +56,85 @@ list<int> directed_graph::bfs(int source, int target_init, int target_end) {
 
 	queue<int> cola;
 	int cant_nodos = graph_container.size();
-	int visitados[cant_nodos];
-	int padres[cant_nodos];
 
-	// Se inicializan a todos como no visitados y sin padre
+	/* Arreglo donde se guarda por cada nodo un flag que indica si ya lo visite */
+	int visitados[cant_nodos];
+	/* Arreglo donde se guarda el padre de cada nodo para reconstruir el camino */
+	int padres[cant_nodos];
+	// nodo target encontrado, ya que el destino pueden ser varios k+1 nodos
+	int nodo_target_encontrado = -1;
+	// Camino encontrado
+	list<int> camino;
+
+	/* Antes de comenzar con todo el procedimiendo preguntamos si source es algun target
+	*  y de ser asi salgo enlistando el nodo en la solucion.
+	*/
+	if(source >= target_init && source <= target_end) {
+		camino.push_back(source);
+		return camino;
+	}
+
+
+	/* Se inicializan a todos como no visitados y sin padre */
 	for(int i=0; i<cant_nodos; i++) {
 		visitados[i] = 0;
 		padres[i] = -1;
 	}
 
-	cola.push(source);
-	visitados[source] = 1;
-	// fix temporal porque sino tira error de que padres no se usa
+	/* fix porque sino tira error de que padres no se usa */
 	if(padres[source] == -1) padres[source] = -1;
 
-	while(!cola.empty()) {
+
+
+	/* Comienza la recorrida para buscar algun target */
+	cola.push(source);
+	visitados[source] = 1;
+
+	/* Se recorren todos los nodos a menos que encuentre algun target */
+	while(!cola.empty() && nodo_target_encontrado == -1) {
+
 		int nodo_padre = cola.front();
 		cola.pop();
 
 		list<int>* adyacentes = graph_container[nodo_padre];
+
+		/* Por cada nodo adyacente al actual se corrobora que no haya sido visitado, se lo marca como tal,
+		*  se le asigna un padre y se lo pushea a la cola para recorrer a sus hijos.
+		*/
 		for(list<int>::iterator it = adyacentes->begin(); it != adyacentes->end(); ++it) {
+
 			int nodo_hijo = *it;
+
+			/* Solo lo analizo si no fue visitado previamente */
 			if(visitados[nodo_hijo] == 0) {
+				
+				/* Se marca al nodo como visitado para evitar recorrerlo de nuevo, se le asigna padre
+				*  para reconstruir el camino al source y se lo pushea para recorrer a los hijos.
+				*/
 				visitados[nodo_hijo] = 1;
 				padres[nodo_hijo] = nodo_padre;
 				cola.push(nodo_hijo);
+
+				/* Si encontre algun target dejo de iterar y guardo cual de ellos encontre */
+				if(nodo_hijo >= target_init && nodo_hijo <= target_end) {
+					nodo_target_encontrado = nodo_hijo;
+					break;
+				}
+
 			}
 		}
 	}
 
 	
-	// Camino encontrado
-	list<int> camino;
-
-	// Se imprime el camino para llegar del source al target_init
-	if(visitados[target_init] != 0) {
-
-		int nodo = target_init;
-		
-		camino.push_back(nodo);
-		while(nodo != source) {
-			nodo = padres[nodo];
-			camino.push_back(nodo);
-		}
-		
-		
-	}
+	/* Comenzando del nodo_target_encontrado se arma el camino hasta el source siguiendo a los padres */
+	int nodo = nodo_target_encontrado;
 	
+	camino.push_back(nodo);
+	while(nodo != source) {
+		nodo = padres[nodo];
+		camino.push_back(nodo);
+	}
+
 	return camino;
 
 }
