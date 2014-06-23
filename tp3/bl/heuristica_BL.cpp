d39900aceeb36acc9c2dd64b71d0d8e7e9c5cb4a
Matias Guerson <matias.guerson@gmail.com>
2014-06-23 20:38:34 -0300

    Se agrega la impresión de la salida acorde a la consigna

--- tp3/bl/heuristica_BL.cpp
+++ tp3/bl/heuristica_BL.cpp
@@ -10,8 +10,9 @@ using namespace std;
 
 bool pesoEnRegla(vector<int> camino, vector<vector<double> > pesos);
 double getPeso(vector<int> camino, vector<vector<double> > pesos);
-vector<int> switchTramo(vector<int> camino1, vector<int> tramoNuevo, int nodoSource, int nodoTarget);
+vector<int> switchTramo(vector<int> camino1, vector<int> tramoNuevo, int nodoSource);
 void imprimirCamino(vector<int> camino);
+void imprimirSolucion(vector<int> camino, vector<vector<double> > pesos1, vector<vector<double> > pesos2);
 double k;
 graph* grafo;
 
@@ -48,16 +49,12 @@ int main(int argc, char* argv[]) {
 
 	vector<vector<double> > pesos1 = grafo->get_weights1();
 	vector<vector<int> > floyd1 = algoritmo->floyd(pesos1);
-	//cout << "PESOS: " << endl;
-	//for(unsigned int i=0; i<pesos1.size(); i++) {
-	//	for(unsigned int j=0; j<pesos1.size(); j++) {
-	//		cout << pesos1[i][j] << " ";
-	//	}
-	//	cout << endl;
-	//}
 
+	/* Generacion del camino minimo para w1 */
 	vector<int> camino1 = algoritmo->reconstruirPathFloyd(u,v,floyd1);
 
+	/* Si este peso supera el K entonces no voy a encontrar uno menor,
+		por lo tanto no hay solucion */
 	if(!pesoEnRegla(camino1,pesos1)) {
 		cout << "no" << endl;
 		return 0;
@@ -65,11 +62,14 @@ int main(int argc, char* argv[]) {
 
 	vector<vector<double> > pesos2 = grafo->get_weights2();
 	vector<vector<int> > floyd2 = algoritmo->floyd(pesos2);
-
+	
+	/* Se recorre todo el camino c1 y para cada par de nodos se va reemplazando el tramo por el 
+		camino minimo entre ese par de nodos para w2. En cada iteracion si no se supera el 
+		valor de K entonces se realiza el reemplazo.*/
 	unsigned int j = 0;
 	while(j!=camino1.size() - 1) {
 		vector<int> tramoCamino2 = algoritmo->reconstruirPathFloyd(camino1[j],camino1[j+1], floyd2);
-		vector<int> caminoNuevo = switchTramo(camino1, tramoCamino2, j, j+1);
+		vector<int> caminoNuevo = switchTramo(camino1, tramoCamino2, j);
 		if(pesoEnRegla(caminoNuevo,pesos1)) {
 			camino1 = caminoNuevo;
 			j += tramoCamino2.size() - 1;
@@ -77,7 +77,7 @@ int main(int argc, char* argv[]) {
 		j++;
 	}
 
-	imprimirCamino(camino1);
+	imprimirSolucion(camino1, pesos1, pesos2);
 
 	return 0;
 }
@@ -95,19 +95,23 @@ double getPeso(vector<int> camino, vector<vector<double> > pesos) {
 	return pesoTotal;
 }
 
-vector<int> switchTramo(vector<int> camino1, vector<int> tramoNuevo, int nodoSource, int nodoTarget) {
+vector<int> switchTramo(vector<int> camino1, vector<int> tramoNuevo, int nodoSource) {
 	unsigned int sizeCamino1 = camino1.size();
 	unsigned int sizeTramoNuevo = tramoNuevo.size();
 
 	vector<int> caminoNuevo = vector<int>(sizeCamino1 + sizeTramoNuevo - 1, 0);
+
+	/* El caminoNuevo se mantiene igual que camino1 hasta la posicion indicada como nodoSource */
 	for(int i=0; i<=nodoSource; i++) {
 		caminoNuevo[i] = camino1[i];
 	}
 
+	/* A partir del nodoSource en adelante se agrega el tramoNuevo */
 	for(int i=nodoSource+1; i<(nodoSource + sizeTramoNuevo); i++) {
 		caminoNuevo[i] = tramoNuevo[i-nodoSource-1];
 	}
 
+	/* Una vez que se completo el tramoNuevo se siguen agregando las ultimas posiciones del camino1 */
 	for(unsigned int i=nodoSource+sizeTramoNuevo; i<caminoNuevo.size(); i++) {
 		caminoNuevo[i] = camino1[i-sizeTramoNuevo+1];
 	}
@@ -118,7 +122,13 @@ vector<int> switchTramo(vector<int> camino1, vector<int> tramoNuevo, int nodoSou
 void imprimirCamino(vector<int> camino) {
 	for(vector<int>::const_iterator it = camino.begin(); it != camino.end(); ++it)
 		cout << *it << " ";
-	cout << endl;
 }
 
-
+void imprimirSolucion(vector<int> camino, vector<vector<double> > pesos1, vector<vector<double> > pesos2) {
+	double pesoTotal1 = getPeso(camino, pesos1);
+	double pesoTotal2 = getPeso(camino, pesos2);
+	
+	cout << pesoTotal1 << " " << pesoTotal2 << " " << camino.size() << " ";
+	imprimirCamino(camino);
+	cout << endl;
+}
